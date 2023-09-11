83ec703e4e1b9920ede5579ab2d58a0d76bf1bad
Franki Callard <francescalily@outlook.com>
2023-09-11 16:10:21 +0100

    profile page with profile card/dog level component

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import ProfileCard from "../../components/ProfileCard";
+import { DogCard, DogLevel } from "../../components";
 
-const Profile = () => {
+function Profile() {
+  const user = {
+    profileImage: "https://via.placeholder.com/150",
+    firstName: "Tim",
+    lastName: "Rowe",
+    email: "tim.row@example.com",
+  };
+
   return (
-    <div>Profile</div>
-  )
+    <div className="profile-container">
+      <ProfileCard user={user} />
+      <div className="dog-container">
+        <h1>Your Dog</h1>
+        <DogCard />
+        <DogLevel />
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
