f0b787d305e9adaf88da13f73eb82851dca9f1c0
Franki Callard <francescalily@outlook.com>
2023-09-11 11:13:39 +0100

    base home/lottie component - will change once done routing and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import "@lottiefiles/lottie-player";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div>App</div>
+      <Home />
+    </>
+  );
+};
 
-export default App
+export default App;
