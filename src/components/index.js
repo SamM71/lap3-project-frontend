3b086a3d9cf98afb192dd6f4dfb1cd61ba1b8ae8
Franki Callard <francescalily@outlook.com>
2023-09-11 14:39:34 +0100

    NavBar test

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as NavBar } from './NavBar'
-export { default as Animations } from './Animations'
+export { default as NavBar } from "./NavBar";
+export { default as Animations } from "./Animations";
 // export { default as LottiePlayer } from './LottiePlayer'
