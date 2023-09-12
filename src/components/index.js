4634ea8170ba5a872fb1f7888611a8af10b91a7d
SamM71 <sammocock@gmail.com>
2023-09-12 14:18:51 +0100

    separate walking and sleeping animations

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,5 @@ export { default as DogLevel } from "./DogLevel";
 export { default as Timer } from './Timer'
 export { default as StartButton } from './StartButton'
 export { default as PauseButton } from './PauseButton'
+export { default as WalkingAnimation } from './WalkingAnimation'
+export { default as SleepingAnimation } from './SleepingAnimation'
