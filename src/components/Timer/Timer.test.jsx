de3c251ce989e500e7c0366a1a8d87335028f6e8
SamM71 <sammocock@gmail.com>
2023-09-12 11:54:07 +0100

    finally fix tests

--- src/components/Timer/Timer.test.jsx
+++ src/components/Timer/Timer.test.jsx
@@ -1,24 +1,24 @@
-// import React from "react";
-// import { describe, it, expect, beforeEach, afterEach } from "vitest";
-// import { screen, render, cleanup } from "@testing-library/react";
-// import matchers from "@testing-library/jest-dom/matchers";
-// expect.extend(matchers);
-// import Timer from ".";
+import React from "react";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { screen, render, cleanup } from "@testing-library/react";
+import matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
+import Timer from ".";
 
-// describe("NavBar component", () => {
-//   beforeEach(() => {
-//     render(
-//       <Timer />
-//     );
-//   });
+describe("NavBar component", () => {
+  beforeEach(() => {
+    render(
+      <Timer />
+    );
+  });
 
-//   it("Displays a progressbar", () => {
-//     const progressbar = screen.getByText("NaN:NaN")
+  it("Displays a progressbar", () => {
+    const progressbar = screen.getByText("NaN:NaN")
 
-//     expect(progressbar).toBeInTheDocument();
-//   });
+    expect(progressbar).toBeInTheDocument();
+  });
 
-//   afterEach(() => {
-//     cleanup();
-//   });
-// });
+  afterEach(() => {
+    cleanup();
+  });
+});
