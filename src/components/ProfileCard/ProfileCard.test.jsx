344e49cd5a410f60e8f0726a275e1f5eb67673e5
Franki Callard <francescalily@outlook.com>
2023-09-12 09:30:11 +0100

    two tests monday

---
+++ src/components/ProfileCard/ProfileCard.test.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { screen, render, cleanup } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+import matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
+import ProfileCard from ".";
+
+describe("ProfileCard component", () => {
+  const user = {
+    profileImage: "https://via.placeholder.com/150",
+    firstName: "Tim",
+    lastName: "Rowe",
+    email: "tim.row@example.com",
+  };
+
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <ProfileCard user={user} />
+      </BrowserRouter>
+    );
+  });
+
+  it("Displays the user profile details correctly", () => {
+    const img = screen.getByRole("img", { name: /tim rowe/i });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute("src", user.profileImage);
+
+    const name = screen.getByText(/tim rowe/i);
+    expect(name).toBeInTheDocument();
+
+    const email = screen.getByText(/tim.row@example.com/i);
+    expect(email).toBeInTheDocument();
+
+    const button = screen.getByRole("button", { name: /see tasks completed/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+});
