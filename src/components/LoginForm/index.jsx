1f82b8d634c499b52e456596a85359429efec4c6
Franki Callard <francescalily@outlook.com>
2023-09-13 20:06:40 +0100

    gtting there dont make pull request yet

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,37 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import LottiePlayer from "../LottiePlayer";
 import { useForm } from "react-hook-form";
 
-
 const LoginForm = () => {
-
-
   const { register, handleSubmit } = useForm();
   const [data, setData] = useState("");
 
   return (
     <div className="grid-container">
-         <div className='left'>
-            <LottiePlayer
-            src="https://lottie.host/a45a8085-dab9-4bf7-8aee-35a38ebf4e42/aDFz0w3EDb.json"             // background="#FFFFFF"
-             speed="0.8"
-             style={{ width: "auto", height: "400px" }}
-             loop
-           autoplay
-            direction="1"
-           mode="normal"
-       />
+      <div className="left">
+        <LottiePlayer
+          src="https://lottie.host/a45a8085-dab9-4bf7-8aee-35a38ebf4e42/aDFz0w3EDb.json" // background="#FFFFFF"
+          speed="0.8"
+          style={{ width: "auto", height: "400px" }}
+          loop
+          autoplay
+          direction="1"
+          mode="normal"
+        />
       </div>
-    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-        <label for="email-input">Email</label><br /> 
-        <input {...register("email")} id="email-input" type="email" placeholder="email" />
-        <label for="password-input">New Password</label><br /> 
-        <input {...register("password")} id="password-input" type="password" placeholder="••••••••" />
-      <p>{data}</p>
-      <input type="submit" />
-    </form>
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <label for="email-input">Email</label>
+        <br />
+        <input
+          {...register("email")}
+          id="email-input"
+          type="email"
+          placeholder="email"
+        />
+        <label for="password-input">New Password</label>
+        <br />
+        <input
+          {...register("password")}
+          id="password-input"
+          type="password"
+          placeholder="••••••••"
+        />
+        <p>{data}</p>
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,46 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import LottiePlayer from "../LottiePlayer";
 import { useForm } from "react-hook-form";
-import './register.css';
-
+import "./register.css";
+import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
-//   const [inputVal, setInputVal] = useState("")
-//   const [displayVal, setDisplayVal] = useState(false)
+  //   const [inputVal, setInputVal] = useState("")
+  //   const [displayVal, setDisplayVal] = useState(false)
 
   const { register, handleSubmit } = useForm();
   const [data, setData] = useState("");
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    try {
+      console.log(JSON.stringify(data));
+      const response = await fetch("http://localhost:8080/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+
+      const responseData = await response.json();
+      console.log("Response Data:", responseData);
+
+      if (responseData.token) {
+        console.log("Response Data:", responseData);
+        localStorage.setItem("token", responseData.token);
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
     <div className="grid-container">
-         <div className='left'>
-            <LottiePlayer
-            src="https://lottie.host/a45a8085-dab9-4bf7-8aee-35a38ebf4e42/aDFz0w3EDb.json"             // background="#FFFFFF"
-             speed="0.8"
-             style={{ width: "auto", height: "400px" }}
-             loop
-           autoplay
-            direction="1"
-           mode="normal"
-       />
+      <div className="left">
+        <LottiePlayer
+          src="https://lottie.host/a45a8085-dab9-4bf7-8aee-35a38ebf4e42/aDFz0w3EDb.json" // background="#FFFFFF"
+          speed="0.8"
+          style={{ width: "auto", height: "400px" }}
+          loop
+          autoplay
+          direction="1"
+          mode="normal"
+        />
       </div>
-    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-      {/*<Header />*/}
-      <label for="name-input">Your Name</label><br /> 
-      <input {...register("name")} id="name-input" type="text" placeholder="Name" />
-      <label for="username-input">Username</label><br /> 
-      <input {...register("username")} id="username-input" type="text" placeholder="username" />
-      <label for="email-input">Email</label><br /> 
-      <input {...register("email")} id="email-input" type="email" placeholder="email" />
-      <label for="new-password-input">New Password</label><br /> 
-      <input {...register("password")} id="new-password-input" type="password" placeholder="••••••••" />
-      <label for="confirm-password-input">Confirm Password</label><br /> 
-      <input {...register("password")} id="confirm-password-input" type="password" placeholder="••••••••" />
-      <p>{data}</p>
-      <input type="submit" />
-    </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/*<Header />*/}
+        <label for="name-input">Your Name</label>
+        <br />
+        <input
+          {...register("name")}
+          id="name-input"
+          type="text"
+          placeholder="Name"
+        />
+        <label for="username-input">Username</label>
+        <br />
+        <input
+          {...register("username")}
+          id="username-input"
+          type="text"
+          placeholder="username"
+        />
+        <label for="email-input">Email</label>
+        <br />
+        <input
+          {...register("email")}
+          id="email-input"
+          type="email"
+          placeholder="email"
+        />
+        <label for="new-password-input">New Password</label>
+        <br />
+        <input
+          {...register("password")}
+          id="new-password-input"
+          type="password"
+          placeholder="••••••••"
+        />
+        <label for="confirm-password-input">Confirm Password</label>
+        <br />
+        <input
+          {...register("password")}
+          id="confirm-password-input"
+          type="password"
+          placeholder="••••••••"
+        />
+        <p>{data}</p>
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
