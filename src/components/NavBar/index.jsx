f0a1823b962f51569d5f0552f14889e48d6cf5eb
rantirules <oa740@nyu.edu>
2023-09-14 18:24:54 +0100

    updated navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import './index.css'
 
-const styles = ({ isActive }) => ({ color: isActive ? 'white' : '#31384a', backgroundColor: isActive? '#31384a' : 'transparent'});
+const styles = ({ isActive }) => ({ color: isActive ? 'white' : 'white', backgroundColor: isActive? '#31384a' : 'transparent'});
 
 const NavBar = () => {
   return (
     <>
       <header>
         <nav id='navBar'>
+          <img src="../../../public/pawlogo.png" width="2%" margin-right="2%"/>
+          <p>Pomodogo</p>
           <NavLink to="/profile" style={styles} className={"nav-links"}>Profile</NavLink>
           <NavLink to="/pomodoro" style={styles} className={"nav-links pomo-page"}>Pomodoro</NavLink>
           <NavLink to="/logout" id="logout-btn" className={"logout"}>Logout</NavLink>
         </nav>
       </header>
       <Outlet />
-      <footer>Made by Mongo-Juice</footer>
+      <footer id="footer-col">Made by Mongo-Juice 🧃</footer>
     </>
   )
 }
