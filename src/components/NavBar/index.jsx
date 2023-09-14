bbf9f9634d37aee4aafc84a8859ee55d0165393a
rantirules <oa740@nyu.edu>
2023-09-14 20:33:36 +0100

    updated nav

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import './index.css'
 
-const styles = ({ isActive }) => ({ color: isActive ? 'white' : 'white', backgroundColor: isActive? '#31384a' : 'transparent'});
+const styles = ({ isActive }) => ({ color: isActive ? 'white' : '#ffffff', backgroundColor: isActive? '#31384a' : 'transparent'});
 
 const NavBar = () => {
   return (
     <>
       <header>
         <nav id='navBar'>
-          <img src="../../../public/pawlogo.png" width="2%" margin-right="2%"/>
-          <p>Pomodogo</p>
+          <div id="imageBox">
+            <img src="../../../public/pawlogo.png" width="20px"/>
+            <p>PomoDogo</p>
+          </div>
           <NavLink to="/profile" style={styles} className={"nav-links"}>Profile</NavLink>
           <NavLink to="/pomodoro" style={styles} className={"nav-links pomo-page"}>Pomodoro</NavLink>
           <NavLink to="/logout" id="logout-btn" className={"logout"}>Logout</NavLink>
