a23ba4f18c21548062adefc91b34830fe75d22e6
SamM71 <sammocock@gmail.com>
2023-09-15 11:08:40 +0100

    try new dist src

--- src/components/EndTimerButton/index.jsx
+++ src/components/EndTimerButton/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EndTimerButton = (props) => {
   return (
     <button {...props} className='timer-btns'>
-      <img src="../dist/stopbtn.png" alt="Stop" width="50"/>
+      <img src="../../dist/stopbtn.png" alt="Stop" width="50"/>
     </button>
   )
 }
